92ae359237fbeffec1328c5058675bb07d9e6dce
akash2003git <akash.tayade.dev@gmail.com>
2025-03-09 19:59:30 +0530

    Updates

--- 1_basics/5_basic_recursion/7_valid_palindrome.cpp
+++ 1_basics/5_basic_recursion/7_valid_palindrome.cpp
@@ -6,35 +6,35 @@ using namespace std;
 
 class Solution {
 public:
-  bool isPalindrome(string s) {
-    // if (i == 0) {
-    //   string result = "";
-    //   for (char c : s) {
-    //     if (isalnum(c)) {
-    //       result += tolower(c);
-    //     }
-    //   }
-    //   s = result;
-    // }
-    // if (i >= s.size() / 2) {
-    //   return true;
-    // }
-    // if (s[i] != s[s.size() - 1 - i]) {
-    //   return false;
-    // }
-    // return isPalindrome(s, i + 1);
-    int l = 0, r = s.size() - 1;
-    while (l < r) {
-      while (l < r && !isalnum(s[l]))
-        l++;
-      while (l < r && !isalnum(s[r]))
-        r--;
-      if (tolower(s[l]) != tolower(s[r]))
-        return false;
-      l++;
-      r--;
+  bool isPalindrome(string s, int i = 0) {
+    if (i == 0) {
+      string result = "";
+      for (char c : s) {
+        if (isalnum(c)) {
+          result += tolower(c);
+        }
+      }
+      s = result;
     }
-    return true;
+    if (i >= s.size() / 2) {
+      return true;
+    }
+    if (s[i] != s[s.size() - 1 - i]) {
+      return false;
+    }
+    return isPalindrome(s, i + 1);
+    // int l = 0, r = s.size() - 1;
+    // while (l < r) {
+    //   while (l < r && !isalnum(s[l]))
+    //     l++;
+    //   while (l < r && !isalnum(s[r]))
+    //     r--;
+    //   if (tolower(s[l]) != tolower(s[r]))
+    //     return false;
+    //   l++;
+    //   r--;
+    // }
+    // return true;
   }
 };
 
