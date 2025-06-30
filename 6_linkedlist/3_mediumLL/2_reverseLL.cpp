e89c30af92be4cd4353baedb7cbfa4f471d28459
akash2003git <akash.tayade.dev@gmail.com>
2025-06-30 21:18:37 +0530

    Medium LL Problems

--- 6_linkedlist/3_mediumLL/2_reverseLL.cpp
+++ 6_linkedlist/3_mediumLL/2_reverseLL.cpp
@@ -12,6 +12,9 @@ struct ListNode {
 
 class Solution {
 public:
+  // Brute Force
+  // TC: O(2N)
+  // SC: O(N)
   ListNode *reverseListBrute(ListNode *head) {
     if (head == nullptr || head->next == nullptr)
       return head;
@@ -31,13 +34,29 @@ public:
     return head;
   }
 
-  ListNode *reverseList(ListNode *head) {
+  // Optimal - I
+  // TC: O(N)
+  // SC: O(1)
+  ListNode *reverseListIterative(ListNode *head) {
     if (head == nullptr || head->next == nullptr)
       return head;
     ListNode *back = head;
     ListNode *temp = head->next;
     ListNode *front = temp->next;
     return head;
+  }
+
+  // Optimal - II(recursive)
+  // TC: O(N)
+  // SC: O(1)
+  ListNode *reverseList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr)
+      return head;
+    ListNode *newHead = reverseList(head->next);
+    ListNode *front = head->next;
+    front->next = head;
+    head->next = nullptr;
+    return newHead;
   }
 };
 
